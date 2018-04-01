083976e29bf9fd88e5d688bace859c5775ac372c
PanZhichen <zhchpan@163.com>
2018-04-01 12:03:28 +0800

    Spliting the PointCloud Map into segments and storing the Map as a '.pcd' file once Loop is detected

--- src/pointcloudRegister.cpp
+++ src/pointcloudRegister.cpp
@@ -23,12 +23,14 @@ using namespace std;
 const double PI = 3.1415926;
 
 pcl::PointCloud<pcl::PointXYZ>::Ptr surroundCloud(new pcl::PointCloud<pcl::PointXYZ>());
+pcl::PointCloud<pcl::PointXYZ>::Ptr surroundLoopCloud(new pcl::PointCloud<pcl::PointXYZ>());
 pcl::PointCloud<pcl::PointXYZ>::Ptr syncCloud(new pcl::PointCloud<pcl::PointXYZ>());
 pcl::PointCloud<pcl::PointXYZ>::Ptr tempCloud(new pcl::PointCloud<pcl::PointXYZ>());
 
-const int downSizeMap = 10;
+const int downSizeMap = 15;
 
 ros::Publisher *surroundCloudPubPointer = NULL;
+ros::Publisher surroundLoopPub;
 vector<ros_orb_slam2::PointCloudWithKF> PCwithKF_vec;
 
 void PCwithKFDataHandler(const ros_orb_slam2::PointCloudWithKFConstPtr& msg)
@@ -42,7 +44,7 @@ void PCwithKFDataHandler(const ros_orb_slam2::PointCloudWithKFConstPtr& msg)
     
     pcl::VoxelGrid<pcl::PointXYZ> downSizeFilter;
     downSizeFilter.setInputCloud(syncCloud);
-    downSizeFilter.setLeafSize(0.3, 0.3, 0.3);
+    downSizeFilter.setLeafSize(0.5, 0.5, 0.5);
     downSizeFilter.filter(*tempCloud);
     syncCloud->clear();
     
@@ -71,20 +73,20 @@ void PCwithKFDataHandler(const ros_orb_slam2::PointCloudWithKFConstPtr& msg)
 //       surroundCloud->push_back(syncCloud->points[idx]);
 //     }
     
-    static int downSizeCount=0;
-    downSizeCount++;
-    
-    if(downSizeCount >= downSizeMap){
-      downSizeCount=0;
-      tempCloud->clear();
-      pcl::VoxelGrid<pcl::PointXYZ> downSizeMapFilter;
-      downSizeMapFilter.setInputCloud(surroundCloud);
-      downSizeMapFilter.setLeafSize(0.3,0.3,0.3);
-      downSizeMapFilter.filter(*tempCloud);
-      pcl::PointCloud<pcl::PointXYZ>::Ptr exchange=surroundCloud;
-      surroundCloud=tempCloud;
-      tempCloud=exchange;
-    }
+//     static int downSizeCount=0;
+//     downSizeCount++;
+//     
+//     if(downSizeCount >= downSizeMap){
+//       downSizeCount=0;
+//       tempCloud->clear();
+//       pcl::VoxelGrid<pcl::PointXYZ> downSizeMapFilter;
+//       downSizeMapFilter.setInputCloud(surroundCloud);
+//       downSizeMapFilter.setLeafSize(0.3,0.3,0.3);
+//       downSizeMapFilter.filter(*tempCloud);
+//       pcl::PointCloud<pcl::PointXYZ>::Ptr exchange=surroundCloud;
+//       surroundCloud=tempCloud;
+//       tempCloud=exchange;
+//     }
 
     sensor_msgs::PointCloud2 surroundCloud2;
     pcl::toROSMsg(*surroundCloud, surroundCloud2);
@@ -93,7 +95,7 @@ void PCwithKFDataHandler(const ros_orb_slam2::PointCloudWithKFConstPtr& msg)
     surroundCloudPubPointer->publish(surroundCloud2);
     }
     else{
-      surroundCloud->clear();
+      surroundLoopCloud->clear();
       uint count=0;
       for(uint idx0=1;idx0<msg->KF_ID.data.size();idx0++){
 	uint64_t KF_ID_L = msg->KF_ID.data[idx0];
@@ -126,49 +128,45 @@ void PCwithKFDataHandler(const ros_orb_slam2::PointCloudWithKFConstPtr& msg)
 	      point = tempCloud->points[i];
 	      double pointDis = sqrt(point.x * point.x + point.y * point.y + point.z * point.z);
 
-	      if (pointDis > 0.3 && pointDis < 50 && point.z<9.0) {
+	      if (pointDis > 0.3 && pointDis < 50 && point.z<10.0) {
 		tf::Vector3 p_wc = tf::Matrix3x3(q_wc) * tf::Vector3(point.x,point.y,point.z) + t_wc;
 		point.x = p_wc.getX();
 		point.y = p_wc.getY();
 		point.z = p_wc.getZ();
-		surroundCloud->push_back(point);
+		surroundLoopCloud->push_back(point);
 	      }
 	    }
 	  }
 	}
       }
 
-      std::cout<<"\033[33m KF Size="<<msg->KF_ID.data.size()<<"!!!!!"<<"\033[0m"<<std::endl;
-      std::cout<<"\033[33m KF Found="<<count<<"!!!!!"<<"\033[0m"<<std::endl;
+      std::cout<<"\033[33m After Map updating, There are "<<"\033[31m"<<msg->KF_ID.data.size()<<"\033[33m"<<" KeyFrames totally. \033[0m"<<std::endl;
+      std::cout<<"\033[33m And "<<"\033[31m"<<count<<"\033[33m"<<" KeyFrames can be found in Current PointCloud Map."<<"\033[0m"<<std::endl;
       tempCloud->clear();
       pcl::VoxelGrid<pcl::PointXYZ> downSizeMapFilter;
-      downSizeMapFilter.setInputCloud(surroundCloud);
+      downSizeMapFilter.setInputCloud(surroundLoopCloud);
       downSizeMapFilter.setLeafSize(0.15,0.15,0.15);
       downSizeMapFilter.filter(*tempCloud);
-      surroundCloud->clear();
+      surroundLoopCloud->clear();
+      pcl::PointCloud<pcl::PointXYZ>::Ptr exchange=surroundLoopCloud;
+      surroundLoopCloud=tempCloud;
+      tempCloud=exchange;
       
       string path = "/home/nrsl/orb_ws/PointCloudMap";
       static uint countPC = 0;
       countPC++;
       ostringstream oss;
       oss << path << countPC << ".pcd";
-      pcl::io::savePCDFileASCII (oss.str(), *tempCloud);
-      
-      downSizeMapFilter.setInputCloud(tempCloud);
-      downSizeMapFilter.setLeafSize(0.5,0.5,0.5);
-      downSizeMapFilter.filter(*surroundCloud);
-      tempCloud->clear();
-//       pcl::PointCloud<pcl::PointXYZ>::Ptr exchange=surroundCloud;
-//       surroundCloud=tempCloud;
-//       tempCloud=exchange;
+      pcl::io::savePCDFileASCII (oss.str(), *surroundLoopCloud);
 
       sensor_msgs::PointCloud2 surroundCloud2;
-      pcl::toROSMsg(*surroundCloud, surroundCloud2);
+      pcl::toROSMsg(*surroundLoopCloud, surroundCloud2);
       surroundCloud2.header.frame_id = "odom"; 
       surroundCloud2.header.stamp = msg->KF_Pose.header.stamp;
-      surroundCloudPubPointer->publish(surroundCloud2);
-    }
-    
+      surroundLoopPub.publish(surroundCloud2);
+      surroundLoopCloud->clear();
+      surroundCloud->clear();
+    }  
 }
 
 
@@ -181,8 +179,38 @@ int main(int argc, char** argv)
 
   ros::Publisher surroundCloudPub = nh.advertise<sensor_msgs::PointCloud2> ("/point_cloud_map", 2);
   surroundCloudPubPointer = &surroundCloudPub;
+  surroundLoopPub = nh.advertise<sensor_msgs::PointCloud2> ("/Surround_LOOP_Map", 1);
+  
+  ros::Rate loop_rate(60);
+  while(ros::ok())
+  {
+    ros::spinOnce();
+    
+    if(surroundCloud->size()>8000){
+      tempCloud->clear();
+      pcl::VoxelGrid<pcl::PointXYZ> downSizeMapFilter;
+      downSizeMapFilter.setInputCloud(surroundCloud);
+      downSizeMapFilter.setLeafSize(0.5,0.5,0.5);
+      downSizeMapFilter.filter(*tempCloud);
+      surroundCloud->clear();
+      
+      uint surrNum = tempCloud->size();
+      
+      for(uint i=0;i<surrNum;i++){
+	surroundLoopCloud->push_back(tempCloud->points[i]);
+      }
+      
+      sensor_msgs::PointCloud2 surroundCloudloop;
+      pcl::toROSMsg(*surroundLoopCloud, surroundCloudloop);
+      surroundCloudloop.header.frame_id = "odom"; 
+      surroundCloudloop.header.stamp = ros::Time::now();
+      surroundLoopPub.publish(surroundCloudloop);
+    }
+    
+    loop_rate.sleep();
+  }
 
-  ros::spin();
+//   ros::spin();
 
   return 0;
 }
